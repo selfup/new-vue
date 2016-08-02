8b7dfe11aa6b86fb3ea3a701f9df43408ba19b3a
Regis <boudinot.regis@yahoo.com>
2016-08-01 20:12:18 -0600

    first vue app - half of idea box in an hour - wow

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h2>Welcome to my first Vue App!</h2>
+    <div>
+      <ideas></ideas>
+    <div>
+  </div>
+</template>
+
+<script>
+  import Ideas from './components/Ideas'
+
+  export default {
+    components: {
+      Ideas
+    }
+  }
+</script>
+
+<style>
+  html {
+    height: 100vh;
+  }
+
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  #app {
+    color: #2c3e50;
+    max-width: 600px;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    text-align: center;
+  }
+
+  #app a {
+    color: #42b983;
+    text-decoration: none;
+  }
+</style>
---
+++ src/components/Ideas.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class='container'>
+    Title: <br><input v-model="title" v-on:keyup.enter="addidea">
+    <br><br>
+    Body: <br><input v-model="body" v-on:keyup.enter="addidea">
+    <div v-for="idea in ideas">
+      <h3>{{ idea.title }}</h3>
+      <h3>{{ idea.body }}</h3>
+      <h3>{{ idea.quality }}</h3>
+      <button v-on:click="removeidea($index)">Remove idea</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Lspi from 'lspi'
+
+  const lspi = new Lspi()
+
+  const ready = () => {
+    if (!lspi.getRecord('ideas')) {
+      lspi.setRecord('ideas', [])
+      return []
+    } else {
+      return lspi.getRecord('ideas')
+    }
+  }
+
+  export default {
+    data () {
+      return {
+        ideas: ready(),
+        title: '',
+        body: ''
+      }
+    },
+    methods: {
+      addidea () {
+        const title = this.title.trim()
+        const body = this.body.trim()
+        if (title && body) {
+          this.ideas.push({ title: title, body: body })
+          lspi.setRecord('ideas', this.ideas)
+          this.newidea = ''
+        }
+      },
+      removeidea (index) {
+        this.ideas.splice(index, 1)
+        lspi.setRecord('ideas', this.ideas)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: #42b983;
+  }
+
+  .container {
+    position: center, fixed;
+  }
+</style>
