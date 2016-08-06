412c27d783292242683383a53a0864a09dcfe32d
Regis <boudinot.regis@yahoo.com>
2016-08-06 15:32:55 -0600

    a lot of styling - minimal looks - change quality - sorting next

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<template>
-  <div id="app">
-    <h2>Welcome to my first Vue App!</h2>
-    <div>
-      <ideas></ideas>
-    <div>
-  </div>
-</template>
-
 <script>
   import Ideas from './components/Ideas'
 
@@ -17,18 +8,20 @@
   }
 </script>
 
-<style>
-  html, body {margin:0;padding:0}
+<template>
+  <div id="app" class="container">
+    <h2>Idea Box in Vue.js!</h2>
+    <ideas></ideas>
+  </div>
+</template>
 
+<style>
   body {
-    text-align: center;
-    align-items: center;
-    justify-content: center;
+    background: #827784;
   }
 
   #app {
     color: #2c3e50;
-    max-width: 600px;
     font-family: Source Sans Pro, Helvetica, sans-serif;
     text-align: center;
   }
--- src/components/Ideas.vue
+++ src/components/Ideas.vue
@@ -1,23 +1,9 @@
-<template>
-  <div class='container'>
-    Title: <br><input v-model="title" v-on:keyup.enter="addidea">
-    <br><br>
-    Body: <br><input v-model="body" v-on:keyup.enter="addidea">
-    <div v-for="idea in ideas">
-      <h3>{{ idea.title }}</h3>
-      <h3>{{ idea.body }}</h3>
-      <h3>{{ idea.quality }}</h3>
-      <button v-on:click="removeidea($index)">Remove idea</button>
-    </div>
-  </div>
-</template>
-
 <script>
   import Lspi from 'lspi'
 
   const lspi = new Lspi()
 
-  const ready = () => {
+  const initIdeas = () => {
     if (!lspi.getRecord('ideas')) {
       lspi.setRecord('ideas', [])
       return []
@@ -26,12 +12,25 @@
     }
   }
 
+  const qualityUp = {
+    Swill: 'Plausible',
+    Plausible: 'Genius',
+    Genius: 'Genius'
+  }
+
+  const qualityDown = {
+    Swill: 'Swill',
+    Plausible: 'Swill',
+    Genius: 'Plausible'
+  }
+
   export default {
     data () {
       return {
-        ideas: ready(),
+        ideas: initIdeas(),
         title: '',
-        body: ''
+        body: '',
+        quality: ''
       }
     },
     methods: {
@@ -39,25 +38,136 @@
         const title = this.title.trim()
         const body = this.body.trim()
         if (title && body) {
-          this.ideas.push({ title: title, body: body })
+          this.ideas.unshift({ title: title, body: body, quality: 'Swill' })
           lspi.setRecord('ideas', this.ideas)
-          this.newidea = ''
+          this.clearInput()
         }
       },
       removeidea (index) {
         this.ideas.splice(index, 1)
         lspi.setRecord('ideas', this.ideas)
+      },
+      clearInput () {
+        this.title = ''
+        this.body = ''
+      },
+      qualitydown (index) {
+        let currentIdea = this.ideas[index]
+        currentIdea.quality = qualityDown[currentIdea.quality]
+        lspi.setRecord('ideas', this.ideas)
+      },
+      qualityup (index) {
+        let currentIdea = this.ideas[index]
+        currentIdea.quality = qualityUp[currentIdea.quality]
+        lspi.setRecord('ideas', this.ideas)
+      },
+      sortbyquality () {
+        this.ideas = this.ideas.sort()
       }
     }
   }
 </script>
+
+<template>
+  <div class="container">
+    <div class="input-container container">
+      <div v-if="ideas.length > 1">
+        <button
+          class="btn btn-primary" 
+          v-on:click="sortbyquality"
+        >
+        Sort By Quality
+        </button>
+      </div>
+      <h3>Title:</h3>
+      
+      <input 
+        class="form-control" 
+        v-model="title" 
+        v-on:keyup.enter="addidea"
+      >
+
+      <h3>Body:</h3> 
+
+      <input 
+        class="form-control" 
+        v-model="body" 
+        v-on:keyup.enter="addidea"
+      >
+      <button 
+        class="btn btn-success" 
+        v-on:click="addidea"
+      >
+      Submit
+      </button>
+    </div>
+
+    <div v-for="idea in ideas">
+      <div class="idea-container container">
+        <h4>Title:</h4>
+        <h5>{{ idea.title }}</h5>
+        <h4>Body:</h4>
+        <h5>{{ idea.body }}</h5>
+        <h4>
+          Quality
+          <span 
+            class="glyphicon glyphicon-chevron-up up" 
+            v-on:click="qualityup($index)"
+          >
+          </span>
+          <span 
+            class="glyphicon glyphicon-chevron-down down"
+            v-on:click="qualitydown($index)"
+          >
+          </span>
+        </h4>
+        <h5><em>{{ idea.quality }}</em></h5>
+        <button 
+          class="btn btn-danger" 
+          v-on:click="removeidea($index)"
+        >
+        Remove idea
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped>
   h1 {
     color: #42b983;
   }
 
-  .container {
-    position: center;
+  button {
+    border-radius: 0px;
+    margin: 10px;
+  }
+
+  input {
+    border-radius: 0px;
+  }
+
+  .idea-container {
+    background-color: #c6c4c4;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    width: 70%;
+    box-shadow: 2px 2px 5px grey;
+  }
+
+  .input-container {
+    background-color: #fca664;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    width: 90%;
+    box-shadow: 2px 2px 5px grey;
+  }
+
+  .up {
+    color: #5CB85C;
+  }
+
+  .down {
+    color: #C9302C;
   }
 </style>
