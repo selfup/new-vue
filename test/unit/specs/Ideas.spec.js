3651e94c42fa24ae982cc9308e962f4302f2695c
Regis <boudinot.regis@yahoo.com>
2016-08-07 15:59:55 -0600

    npm debug to gitignore cuz testing - Learning how to test Vuejs

--- test/unit/specs/Ideas.spec.js
+++ test/unit/specs/Ideas.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import Hello from 'src/components/Hello'
+import Ideas from 'src/components/Ideas'
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue({
-      template: '<div><hello></hello></div>',
-      components: { Hello }
+      template: '<div><ideas></ideas></div>',
+      components: { Ideas }
     }).$mount()
     expect(vm.$el.querySelector('.hello h1').textContent).to.contain('Hello World!')
   })
