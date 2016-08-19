eb5e2b523a989e404206f0efc05c4b48ccbf357c
Regis <boudinot.regis@yahoo.com>
2016-08-19 11:27:54 -0600

    remove SW call in html

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <script>
   import Ideas from './components/Ideas'
 
-  const registerServiceWorker = () => {
-    const httpsCheck = window.location.href.split('')[4]
-    const lh         = 'localhost:8080'
-
-    if ( httpsCheck !== 's' && !window.location.href.includes(lh)) return
-    if (!navigator.serviceWorker)                                  return
-
-    navigator
-      .serviceWorker
-        .register('/static/service-worker.js')
-          .then(reg => {
-            console.log("Service worker registered")
-              reg
-                .update()
-                  .then(reg => {
-                    console.log("Cached Assets UPDATED")
-                  })
-          })
-  }
-
-  registerServiceWorker()
-
   export default {
     components: {
       Ideas
